588a0738a4ed6a90635620c3a3e9065eaa25fa7b
Rene Meissner <info@opendgps.de>
2018-10-18 14:11:53 +0200

    Create hffd.c

---
+++ src/hffd.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "cuda/hffd.cu"
+
+int decodeHF ( char* inputFileName, char* outputFileName ) {
+  return 0;
+}
+
+int getDeltaRecords ( char* inputMasterFileName, char* inputReferenceFileName, char* outputFileName ) {
+  return 0;
+}
+
+int getDeltaVariables ( char* inputMasterFileName, char* inputReferenceFileName, char* outputFileName ) {
+  return 0;
+}
+
+int main ( void ) {
+  return 0;
+}
