f4ddad9dccb39d5285dd7e364c7188a9182ef3e7
tinag tir <tinag.tir@gmail.com>
2018-10-18 20:32:06 +0200

    Added comp3ToInt

--- src/cuda/hffd.cu
+++ src/cuda/hffd.cu
@@ -2,6 +2,20 @@
 #include <cuda_runtime.h>
 #include <helper_cuda.h>
 
+
+// 03 C86R8200-RECID        PIC 9(05) COMP-3 VALUE ZERO.
+__global__ int comp3ToInt ( int memAddress ) {
+	// converting BCD to integer 
+	// http://www.3480-3590-data-conversion.com/article-bcd-binary.html
+	char byteOne, byteTwo, byteThree, byteFour, byteFive;
+	byteOne = memAddress[0];
+	byteTwo = memAddress[1];
+	byteThree = memAddress[2];
+	byteFour = memAddress[3];
+	byteFive = memAddress[4];
+	return 0;
+}
+
 __global__ char smallInt ( char in ) {
 	return in;
 }
