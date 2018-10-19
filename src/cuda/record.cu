ba9f49f0378402d7b532dc3d5c3f8d5d478ea568
tinag tir <tinag.tir@gmail.com>
2018-10-19 14:58:42 +0200

    Adding kernel code

--- src/cuda/record.cu
+++ src/cuda/record.cu
@@ -10,53 +10,63 @@
 #include "converter.cu"
 #include "writeRecord.cu"
 
+#define RECORDLENGTH 120
 struct recordData
 {
-	// 03 RECID        PIC 9(05) COMP-3 VALUE ZERO.
+	// 03 RECID        PIC 9(05) COMP-3 VALUE ZERO. // 3
 	uint32_t 	RECID;
-	// 03 SYSTEMID     PIC X(04) VALUE 'S085'.
+	// 03 SYSTEMID     PIC X(04) VALUE 'S085'. // 4
 	char		*SYSTEMID[4];
-	// 03 MANDID       PIC 9(05) COMP-3 VALUE 10010.
+	// 03 MANDID       PIC 9(05) COMP-3 VALUE 10010. // 3
 	uint32_t	MANDID;
-	// 03 NAME         PIC X(40) VALUE 'MAX MUSTER'.
+	// 03 NAME         PIC X(40) VALUE 'MAX MUSTER'. // 40
 	char 		*NAME[40];
-	// 03 POLNR        PIC 9(11) COMP-3 VALUE 0100001.
+	// 03 POLNR        PIC 9(11) COMP-3 VALUE 0100001. // 6
 	uint32_t 	POLNR;
-	// 03 RISPA        PIC 9(03) COMP-3 VALUE 207.
+	// 03 RISPA        PIC 9(03) COMP-3 VALUE 207. // 2
 	uint32_t 	RISPA;
-	// 03 WAEHR        PIC X(03) VALUE 'EUR'.
+	// 03 WAEHR        PIC X(03) VALUE 'EUR'. // 3
 	char 		*WAEHR[3];
-	// 03 PRAEMIE      PIC S9(9)V99 COMP-3 VALUE 228.30.
+	// 03 PRAEMIE      PIC S9(9)V99 COMP-3 VALUE 228.30. // 5
 	int32_t		PRAEMIE;
-	// 03 INFO         PIC X(50) VALUE 'ICH BIN EIN SATZ.'.
+	// 03 INFO         PIC X(50) VALUE 'ICH BIN EIN SATZ.'. // 50
 	char 		*INFO[50];
-	// 03 ENDE         PIC X(03) VALUE LOW-VALUE.
+	// 03 ENDE         PIC X(03) VALUE LOW-VALUE. // 3
 	char 		*ENDE[3];
 };
 
+__device__ int getGlobalIdx_1D_1D()
+{
+	return blockIdx.x *blockDim.x + threadIdx.x;
+}
 
-__global__ void decodeRecord ( int numOfCores, int recordNum ) {
-	int inMemAddressBase = 0x00;
+__global__ void decodeRecord ( int numOfCores, int recordNum, uint8_t *inputMemAddress , uint8_t *outputMemAddress ) {
+	int recordAddress;
+	int threadIdx;
+	threadIdx = getGlobalIdx_1D_1D();
+	recordAddress = (threadIdx * RECORDLENGTH);
+	outputMemAddress[threadIdx] = recordAddress; // recordAddress; // inputMemAddress[ ( uint8_t ) recordAddress ];
+
 	struct recordData theRecord;
-	comp3ToInt ( inMemAddressBase, 5, &theRecord.RECID );
-		inMemAddressBase = inMemAddressBase + 5; 
-	charToCharArray ( inMemAddressBase, 4, *theRecord.SYSTEMID );
-		inMemAddressBase = inMemAddressBase + 4; 
-	comp3ToInt ( inMemAddressBase, 5, &theRecord.MANDID );
-		inMemAddressBase = inMemAddressBase + 5; 
-	charToCharArray ( inMemAddressBase, 40, *theRecord.NAME );
-		inMemAddressBase = inMemAddressBase + 40; 
-	comp3ToInt ( inMemAddressBase, 11, &theRecord.POLNR );
-		inMemAddressBase = inMemAddressBase + 11; 
-	comp3ToInt ( inMemAddressBase, 3, &theRecord.RISPA );
-		inMemAddressBase = inMemAddressBase + 3; 
-	charToCharArray ( inMemAddressBase, 3, *theRecord.WAEHR );
-		inMemAddressBase = inMemAddressBase + 3; 
-	comp3ToSignedInt ( inMemAddressBase, 9, &theRecord.PRAEMIE );
-		inMemAddressBase = inMemAddressBase + 9; 
-	charToCharArray ( inMemAddressBase, 50, *theRecord.INFO );
-		inMemAddressBase = inMemAddressBase + 50; 
-	charToCharArray ( inMemAddressBase, 3, *theRecord.ENDE );
-		inMemAddressBase = inMemAddressBase + 3; 
+	comp3ToInt ( recordAddress, 5, &theRecord.RECID );
+		recordAddress = recordAddress + 5; 
+	charToCharArray ( recordAddress, 4, *theRecord.SYSTEMID );
+		recordAddress = recordAddress + 4; 
+	comp3ToInt ( recordAddress, 5, &theRecord.MANDID );
+		recordAddress = recordAddress + 5; 
+	charToCharArray ( recordAddress, 40, *theRecord.NAME );
+		recordAddress = recordAddress + 40; 
+	comp3ToInt ( recordAddress, 11, &theRecord.POLNR );
+		recordAddress = recordAddress + 11; 
+	comp3ToInt ( recordAddress, 3, &theRecord.RISPA );
+		recordAddress = recordAddress + 3; 
+	charToCharArray ( recordAddress, 3, *theRecord.WAEHR );
+		recordAddress = recordAddress + 3; 
+	comp3ToSignedInt ( recordAddress, 9, &theRecord.PRAEMIE );
+		recordAddress = recordAddress + 9; 
+	charToCharArray ( recordAddress, 50, *theRecord.INFO );
+		recordAddress = recordAddress + 50; 
+	charToCharArray ( recordAddress, 3, *theRecord.ENDE );
+		recordAddress = recordAddress + 3; 
 
 }
--- src/hffd.cu
+++ src/hffd.cu
@@ -2,8 +2,71 @@
 #include <stdlib.h>
 #include "cuda/record.cu"
 
-int decodeHF ( char* inputFileName, char* outputFileName ) {
-  return 0;
+#define BINARYSIZE 1560
+#define NUMOFCORES 13
+
+
+int decodeHF ( void ) {
+	uint8_t *ptr_hostfile;
+	uint8_t *ptr_output;
+	FILE *ptr_fp;
+    uint8_t *d_hostfile;
+    uint8_t *d_output;
+	ptr_hostfile = (uint8_t *)malloc(BINARYSIZE);
+	ptr_output = (uint8_t *)malloc(BINARYSIZE);
+	if ( !ptr_hostfile ) {
+		printf("Memory allocation error!\n");
+		exit(1);
+	} 
+	if((ptr_fp = fopen("sample/sample-hostfile.bin", "rb"))==NULL)
+	{
+		printf("Unable to open the file!\n");
+		exit(1);
+	}
+
+	if(fread(ptr_hostfile, BINARYSIZE * sizeof( uint8_t ), 1, ptr_fp) != 1)
+	{
+		printf( "Read error!\n" );
+		exit( 1 );
+	}
+
+	fclose(ptr_fp);
+	free(ptr_hostfile);
+
+	cudaEvent_t start, stop;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+
+	dim3 blocksPerGrid(1,1,1); //use only one block
+	dim3 threadsPerBlock(NUMOFCORES,1,1); //use N threads in the block myKernel<<<blocksPerGrid, threadsPerBlock>>>(result);
+    
+    checkCudaErrors(cudaMalloc((uint8_t**)&d_hostfile, (BINARYSIZE)));
+    checkCudaErrors(cudaMemcpy(d_hostfile, ptr_hostfile, (BINARYSIZE), cudaMemcpyHostToDevice));
+    checkCudaErrors(cudaMalloc((uint8_t**)&d_output, NUMOFCORES * BINARYSIZE));
+    checkCudaErrors(cudaMemcpy(d_output, ptr_output, NUMOFCORES * BINARYSIZE, cudaMemcpyHostToDevice));
+ 	// start the i86 opcode interpreter on the GPU   
+	cudaEventRecord(start);
+    decodeRecord<<<blocksPerGrid, threadsPerBlock>>>(1,1, d_hostfile, d_output); // numofcores and recordnum
+	cudaEventRecord(stop);
+    
+    // checkCudaErrors(cudaMemcpy(ptr_hostfileCopy, d_hostfile, (TOTALBINARYSIZE), cudaMemcpyDeviceToHost));
+    checkCudaErrors(cudaMemcpy(ptr_output, d_output, NUMOFCORES * BINARYSIZE, cudaMemcpyDeviceToHost));
+
+	cudaEventSynchronize(stop);
+	float milliseconds = 0;
+	cudaEventElapsedTime(&milliseconds, start, stop);
+
+
+	for ( int i = 0; i < BINARYSIZE; i++ ) {
+		if ( (i % 30) == 0 ) printf("\n%d\t", i);
+		printf("%02x ", ptr_output[i]);
+	}
+	printf("\n");
+
+	printf("used time in sec: %f\n", milliseconds / 1000);
+	
+	// free(ptr_hostfile);
+	return 0;
 }
 
 int getDeltaRecords ( char* inputMasterFileName, char* inputReferenceFileName, char* outputFileName ) {
@@ -16,5 +79,7 @@ int getDeltaVariables ( char* inputMasterFileName, char* inputReferenceFileName,
 
 int main ( void ) {
 	printf("Starting kernel.\n");
+	decodeHF();
+	printf("Kernel stopped.\n");
   return 0;
 }
