25f32f5c4ceb55e5937d3c6ac0e5a834f3dadfce
tinag tir <tinag.tir@gmail.com>
2018-10-19 12:37:22 +0200

    Renamed hffd.c to hffd.cu

--- src/cuda/converter.cu
+++ src/cuda/converter.cu
@@ -1,20 +1,20 @@
 
-__global__ int comp3ToInt ( int memAddress, int length, int *currentRecordAttr ) {
+__global__ void comp3ToInt ( int memAddress, int length, int *currentRecordAttr ) {
 	// converting BCD to integer 
 	// http://www.3480-3590-data-conversion.com/article-bcd-binary.html
-	return 0;
+	// return 0;
 }
 
-__global__ int comp3ToSignedInt ( int memAddress, int length, int *currentRecordAttr ) {
-	return 0;
+__global__ void comp3ToSignedInt ( int memAddress, int length, int *currentRecordAttr ) {
+	// return 0;
 }
 
-__global__ int charToCharArray ( int memAddress, int length ) {
+__global__ void charToCharArray ( int memAddress, int length ) {
 	// converting ebcdic to ascii
 	// https://stackoverflow.com/questions/7734275/c-code-to-convert-ebcdic-printables-to-ascii-in-place
-	return 0;
+	// return 0;
 }
 
-__global__ char smallInt ( char in ) {
-	return in;
+__global__ void smallInt ( char in ) {
+	// return in;
 }
--- src/cuda/writeRecord.cu
+++ src/cuda/writeRecord.cu
@@ -1,3 +1,3 @@
-__global__ void writeRecordToMemory ( int *currentRecord ) {
+__global__ int writeRecordToMemory ( int *currentRecord ) {
 	return 0;
 }
