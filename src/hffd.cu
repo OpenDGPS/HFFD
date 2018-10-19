227cf754f93d9ed7f13feb770967bcb988b393a8
tinag tir <tinag.tir@gmail.com>
2018-10-19 12:38:34 +0200

    Renamed hffd.c to hffd.cu

---
+++ src/hffd.cu
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "cuda/hffd.cu"
+
+int decodeHF ( char* inputFileName, char* outputFileName ) {
+  return 0;
+}
+
+int getDeltaRecords ( char* inputMasterFileName, char* inputReferenceFileName, char* outputFileName ) {
+  return 0;
+}
+
+int getDeltaVariables ( char* inputMasterFileName, char* inputReferenceFileName, char* outputFileName ) {
+  return 0;
+}
+
+int main ( void ) {
+	printf("Starting kernel.\n");
+  return 0;
+}
